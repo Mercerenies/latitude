d5e03ca70f4033ec581723f82110cd4703176afe
Silvio Mayolo <sdmayolo42@students.tntech.edu>
2017-09-01 22:35:34 -0500

    Environment docs

--- Environment.hpp
+++ Environment.hpp
@@ -4,10 +4,31 @@
 #include <string>
 #include <boost/optional.hpp>
 
+/// \file
+///
+/// \brief Functionality for accessing system environment variables
+
+/// Retrieves the value of the environment variable with the given name.
+///
+/// \param name the name of the environment variable
+/// \return the value of the variable, or boost::none if the variable doesn't exist
 boost::optional<std::string> getEnv(std::string name);
 
-bool setEnv(std::string name, std::string value); // Returns whether supported
+/// Sets the value of the given environment variable. If the variable
+/// does not exist, it is created. This function may not be supported
+/// on unrecognized platforms.
+///
+/// \param name the name of the environment variable
+/// \param value the value to assign to the variable
+/// \return whether or not the operation was supported
+bool setEnv(std::string name, std::string value);
 
-bool unsetEnv(std::string name); // Returns whether supported
+/// Unsets the given environment variable. If the variable does not
+/// exist then this function is a no-op. This function may not be
+/// supported on unrecognized platforms.
+///
+/// \param name the name of the environment variable
+/// \return whether or not the operation was supported
+bool unsetEnv(std::string name);
 
 #endif // ENVIRONMENT_HPP
