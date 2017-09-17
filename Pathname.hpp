98b1872717fed51d4c46ce2c080b44605504e699
Silvio Mayolo <sdmayolo42@students.tntech.edu>
2017-09-16 21:30:48 -0500

    Docs

--- Pathname.hpp
+++ Pathname.hpp
@@ -3,10 +3,36 @@
 
 #include <string>
 
+/// \file
+///
+/// \brief Functions for manipulating and retrieving filenames.
+
+/// Returns the full path and name of the Latitude executable.
+///
+/// Note: This function is only defined if the operating system is
+/// recognized.
+///
+/// \return the path name
 std::string getExecutablePathname();
 
+/// Returns the directory of the argument, which should be an absolute
+/// path.
+///
+/// Note: This function is only defined if the operating system is
+/// recognized.
+///
+/// \param path an absolute path
+/// \return the directory
 std::string stripFilename(std::string path);
 
+/// Returns the filename of the argument, which should be an absolute
+/// path.
+///
+/// Note: This function is only defined if the operating system is
+/// recognized.
+///
+/// \param path an absolute path
+/// \return the filename, without the directory
 std::string stripDirname(std::string path);
 
 #endif // PATHNAME_HPP
