7f2fb52ed29d8669ad0e5cd25ea30f651df4755f
Silvio Mayolo <sdmayolo42@students.tntech.edu>
2018-01-02 23:46:29 -0600

    Docs

--- Stack.hpp
+++ Stack.hpp
@@ -1,20 +1,44 @@
 #ifndef STACK_HPP
 #define STACK_HPP
 
+/// \file
+/// \brief StackNode and NodePtr, as well as the helper methods that
+/// operate on them.
+
 template <typename T>
 class StackNode;
 
+/// A NodePtr is a smart pointer to a StackNode.
 template <typename T>
 using NodePtr = std::shared_ptr<StackNode<T>>;
 
-// A home-baked stack implementation which allows sharing of data between continuations for efficiency
+/// A home-baked linked stack implementation which allows sharing of
+/// data between continuations for efficiency. StackNode sacrifices
+/// spatial locality for the ability to share common tails of
+/// stacks. StackNode instances are immutable, in the sense that push
+/// and pop operations will always return new StackNode instances, so
+/// these objects can be shared across multiple data structures. Note
+/// that StackNode is designed to be wrapped in a NodePtr, so that
+/// `nullptr` designates an empty stack. Thus, a StackNode always
+/// consists of exactly one element and a pointer to the rest of the
+/// stack.
+///
+/// \tparam T the type of elements in the stack
 template <typename T>
 class StackNode {
 private:
     NodePtr<T> next;
     T data;
 public:
+    /// Constructs a stack node containing the single element
+    /// given. The resulting stack will have exactly one element in
+    /// it.
+    ///
+    /// \param data0 the element to place on the stack
     StackNode(const T& data0);
+    /// Returns a reference to the element on the stack.
+    ///
+    /// \return a reference to the stack element
     const T& get();
     template <typename S>
     friend NodePtr<S> pushNode(NodePtr<S> node, const S& data);
@@ -22,9 +46,23 @@ public:
     friend NodePtr<S> popNode(NodePtr<S> node);
 };
 
+/// Adds an element to the stack, returning a modified stack.
+///
+/// \tparam T the type of stack elements
+/// \param node a stack
+/// \param data the element to push
+/// \return the new stack
 template <typename T>
 NodePtr<T> pushNode(NodePtr<T> node, const T& data);
 
+/// Pops an element off the stack, returning a modified stack.
+///
+/// \pre The stack must be nonempty; a `nullptr` argument will result
+/// in undefined behavior
+/// \tparam T the type of stack elements
+/// \param node a stack
+/// \return the new stack
+/// \see StackNode::get()
 template <typename T>
 NodePtr<T> popNode(NodePtr<T> node);
 
