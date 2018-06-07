1a2003dbbc5fe8dfb438082713dc2a330a7e55cf
Silvio Mayolo <sdmayolo42@students.tntech.edu>
2018-06-07 14:42:23 -0500

    Migrated over to a more centralized interface internally

--- src/Serialize.cpp
+++ src/Serialize.cpp
@@ -1,47 +1,36 @@
 
 #include "Serialize.hpp"
 #include <string>
+#include <iterator>
+#include <type_traits>
+
+template <typename Container>
+class PopIterator : public std::iterator<std::input_iterator_tag, typename std::remove_reference<decltype(std::declval<Container>().front())>::type> {
+private:
+    Container& container;
+public:
+    PopIterator(Container& container) : container(container) {}
+    bool operator==(const PopIterator<Container>& other) { return container == other.container; }
+    bool operator!=(const PopIterator<Container>& other) { return !(*this == other); }
+    typename PopIterator::reference operator*() { return container.front(); }
+    typename PopIterator::pointer operator->() { return &container.front(); }
+    PopIterator<Container> operator++() { container.pop_front(); return *this; }
+    // Note: This "iterator" does not support postfix ++ and is
+    // therefore technically not an iterator. It's a temporary patch
+    // for the process of transition and will be removed soon.
+};
+
+template <typename Container>
+PopIterator<Container> popIterator(Container& container) {
+    return { container };
+}
 
 struct AppendVisitor {
     SerialInstrSeq* instructions;
 
-    void operator()(const Reg& reg) {
-        instructions->push_back((unsigned char)reg);
-    }
-
-    void operator()(const std::string& str) {
-        for (char ch : str) {
-            if (ch == 0) {
-                instructions->push_back('\0');
-                instructions->push_back('.');
-            } else {
-                instructions->push_back(ch);
-            }
-        }
-        instructions->push_back('\0');
-        instructions->push_back('\0');
-    }
-
-    void operator()(const long& val) {
-        long val1 = val;
-        if (val1 < 0)
-            instructions->push_back(0xFF);
-        else
-            instructions->push_back(0x00);
-        val1 = abs(val1);
-        for (int i = 0; i < 4; i++) {
-            instructions->push_back((unsigned char)(val1 % 256));
-            val1 /= 256;
-        }
-    }
-
-    void operator()(const FunctionIndex& ind) {
-        // No need for a sign bit; this is an index so it's always nonnegative
-        int val1 = ind.index;
-        for (int i = 0; i < 4; i++) {
-            instructions->push_back((unsigned char)(val1 % 256));
-            val1 /= 256;
-        }
+    template <typename T>
+    void operator()(const T& val) {
+        serialize_t<T>().serialize(val, std::back_inserter(*instructions));
     }
 
 };
@@ -52,64 +41,30 @@ void appendRegisterArg(const RegisterArg& arg, SerialInstrSeq& seq) {
 }
 
 void appendInstruction(const Instr& instr, SerialInstrSeq& seq) {
-    seq.push_back((unsigned char)instr);
+    AppendVisitor visitor { &seq };
+    visitor(instr);
 }
 
 unsigned char popChar(SerialInstrSeq& state) {
-    if (state.empty())
-        return 0;
-    char val = state.front();
-    state.pop_front();
-    return val;
+    return (unsigned char)serialize_t<char>().deserialize(popIterator(state));
 }
 
 long popLong(SerialInstrSeq& state) {
-    int sign = 1;
-    if (popChar(state) > 0)
-        sign *= -1;
-    long value = 0;
-    long pow = 1;
-    for (int i = 0; i < 4; i++) {
-        value += pow * (long)popChar(state);
-        pow <<= 8;
-    }
-    return sign * value;
+    return serialize_t<long>().deserialize(popIterator(state));
 }
 
 std::string popString(SerialInstrSeq& state) {
-    std::string str;
-    unsigned char ch;
-    while (true) {
-        ch = popChar(state);
-        if (ch == '\0') {
-            ch = popChar(state);
-            if (ch == '.')
-                str += '\0';
-            else if (ch == '\0')
-                break;
-        } else {
-            str += ch;
-        }
-    }
-    return str;
+    return serialize_t<std::string>().deserialize(popIterator(state));
 }
 
 Reg popReg(SerialInstrSeq& state) {
-    unsigned char ch = popChar(state);
-    return (Reg)ch;
+    return serialize_t<Reg>().deserialize(popIterator(state));
 }
 
 Instr popInstr(SerialInstrSeq& state) {
-    unsigned char ch = popChar(state);
-    return (Instr)ch;
+    return serialize_t<Instr>().deserialize(popIterator(state));
 }
 
 FunctionIndex popFunction(SerialInstrSeq& state) {
-    int value = 0;
-    int pow = 1;
-    for (int i = 0; i < 4; i++) {
-        value += pow * (long)popChar(state);
-        pow <<= 8;
-    }
-    return { value };
+    return serialize_t<FunctionIndex>().deserialize(popIterator(state));
 }
--- src/Serialize.hpp
+++ src/Serialize.hpp
@@ -3,6 +3,81 @@
 
 #include "Instructions.hpp"
 
+template <typename T>
+struct serialize_t;
+
+template <>
+struct serialize_t<long> {
+
+    using type = long;
+
+    template <typename OutputIterator>
+    void serialize(type arg, OutputIterator iter) const;
+    template <typename InputIterator>
+    type deserialize(InputIterator iter) const;
+
+};
+
+template <>
+struct serialize_t<char> {
+
+    using type = char;
+
+    template <typename OutputIterator>
+    void serialize(type arg, OutputIterator iter) const;
+    template <typename InputIterator>
+    type deserialize(InputIterator iter) const;
+
+};
+
+template <>
+struct serialize_t<Reg> {
+
+    using type = Reg;
+
+    template <typename OutputIterator>
+    void serialize(type arg, OutputIterator iter) const;
+    template <typename InputIterator>
+    type deserialize(InputIterator iter) const;
+
+};
+
+template <>
+struct serialize_t<Instr> {
+
+    using type = Instr;
+
+    template <typename OutputIterator>
+    void serialize(type arg, OutputIterator iter) const;
+    template <typename InputIterator>
+    type deserialize(InputIterator iter) const;
+
+};
+
+template <>
+struct serialize_t<std::string> {
+
+    using type = std::string;
+
+    template <typename OutputIterator>
+    void serialize(const type& arg, OutputIterator iter) const;
+    template <typename InputIterator>
+    type deserialize(InputIterator iter) const;
+
+};
+
+template <>
+struct serialize_t<FunctionIndex> {
+
+    using type = FunctionIndex;
+
+    template <typename OutputIterator>
+    void serialize(const type& arg, OutputIterator iter) const;
+    template <typename InputIterator>
+    type deserialize(InputIterator iter) const;
+
+};
+
 /// Appends an argument to a serialized instruction sequence, as a
 /// sequence of characters.
 ///
@@ -59,5 +134,130 @@ Instr popInstr(SerialInstrSeq& state);
 /// \param state a serial instruction sequence
 /// \return a function index
 FunctionIndex popFunction(SerialInstrSeq& state);
+
+// ----
+
+template <typename OutputIterator>
+auto serialize_t<long>::serialize(type arg, OutputIterator iter) const -> void {
+    long val1 = arg;
+    if (val1 < 0)
+        *iter++ = 0xFF;
+    else
+        *iter++ = 0x00;
+    val1 = abs(val1);
+    for (int i = 0; i < 4; i++) {
+        *iter++ = (unsigned char)(val1 % 256);
+        val1 /= 256;
+    }
+}
+
+template <typename InputIterator>
+auto serialize_t<long>::deserialize(InputIterator iter) const -> type {
+    int sign = 1;
+    if (*iter > 0)
+        sign *= -1;
+    ++iter;
+    long value = 0;
+    long pow = 1;
+    for (int i = 0; i < 4; i++) {
+        value += pow * (long)(*iter);
+        ++iter;
+        pow <<= 8;
+    }
+    return sign * value;
+}
+
+template <typename OutputIterator>
+auto serialize_t<char>::serialize(type arg, OutputIterator iter) const -> void {
+    *iter++ = (unsigned char)arg;
+}
+
+template <typename InputIterator>
+auto serialize_t<char>::deserialize(InputIterator iter) const -> type {
+    char ch = *iter;
+    ++iter;
+    return ch;
+}
+
+template <typename OutputIterator>
+auto serialize_t<Reg>::serialize(type arg, OutputIterator iter) const -> void {
+    *iter++ = (unsigned char)arg;
+}
+
+template <typename InputIterator>
+auto serialize_t<Reg>::deserialize(InputIterator iter) const -> type {
+    unsigned char ch = *iter;
+    ++iter;
+    return (Reg)ch;
+}
+
+template <typename OutputIterator>
+auto serialize_t<Instr>::serialize(type arg, OutputIterator iter) const -> void {
+    *iter++ = (unsigned char)arg;
+}
+
+template <typename InputIterator>
+auto serialize_t<Instr>::deserialize(InputIterator iter) const -> type {
+    unsigned char ch = *iter;
+    ++iter;
+    return (Instr)ch;
+}
+
+template <typename OutputIterator>
+auto serialize_t<std::string>::serialize(const type& arg, OutputIterator iter) const -> void {
+    for (char ch : arg) {
+        if (ch == 0) {
+            *iter++ = '\0';
+            *iter++ = '.';
+        } else {
+            *iter++ = ch;
+        }
+    }
+    *iter++ = '\0';
+    *iter++ = '\0';
+}
+
+template <typename InputIterator>
+auto serialize_t<std::string>::deserialize(InputIterator iter) const -> type {
+    std::string str;
+    unsigned char ch;
+    while (true) {
+        ch = *iter;
+        ++iter;
+        if (ch == '\0') {
+            ch = *iter;
+            ++iter;
+            if (ch == '.')
+                str += '\0';
+            else if (ch == '\0')
+                break;
+        } else {
+            str += ch;
+        }
+    }
+    return str;
+}
+
+template <typename OutputIterator>
+auto serialize_t<FunctionIndex>::serialize(const type& arg, OutputIterator iter) const -> void {
+    // No need for a sign bit; this is an index so it's always nonnegative
+    int val1 = arg.index;
+    for (int i = 0; i < 4; i++) {
+        *iter++ = (unsigned char)(val1 % 256);
+        val1 /= 256;
+    }
+}
+
+template <typename InputIterator>
+auto serialize_t<FunctionIndex>::deserialize(InputIterator iter) const -> type {
+    int value = 0;
+    int pow = 1;
+    for (int i = 0; i < 4; i++) {
+        value += pow * (long)(*iter);
+        ++iter;
+        pow <<= 8;
+    }
+    return { value };
+}
 
 #endif // SERIALIZE_HPP
