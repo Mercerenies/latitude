dc59fcc2feee00c4e895637d7b2355afaf75076c
Silvio Mayolo <sdmayolo42@students.tntech.edu>
2018-07-26 22:37:05 -0500

    Garnish tests

---
+++ test/test_Garnish.cpp
@@ -0,0 +1,69 @@
+//// Copyright (c) 2018 Silvio Mayolo
+//// See LICENSE.txt for licensing details
+
+#include "catch2/catch.hpp"
+#include "Garnish.hpp"
+#include "Proto.hpp"
+#include "Number.hpp"
+#include "Symbol.hpp"
+#include <boost/blank.hpp>
+#include <string>
+
+extern VMState* globalVM;
+
+// Not testing the garnishSeq stuff here right now.
+
+TEST_CASE( "String garnishing", "[garnish]" ) {
+
+  ObjectPtr obj = garnishObject(globalVM->reader, "abc");
+  std::string* str = boost::get<std::string>(&obj->prim());
+
+  REQUIRE( str != nullptr );
+  REQUIRE( *str == "abc" );
+
+}
+
+TEST_CASE( "Number garnishing", "[garnish]" ) {
+
+  ObjectPtr n1 = garnishObject(globalVM->reader, 10);
+  ObjectPtr n2 = garnishObject(globalVM->reader, 10l);
+  ObjectPtr n3 = garnishObject(globalVM->reader, Number((Number::smallint)10));
+
+  Number* x1 = boost::get<Number>(&n1->prim());
+  Number* x2 = boost::get<Number>(&n2->prim());
+  Number* x3 = boost::get<Number>(&n3->prim());
+
+  REQUIRE( x1 != nullptr );
+  REQUIRE( x2 != nullptr );
+  REQUIRE( x3 != nullptr );
+
+  REQUIRE( *x1 == 10l );
+  REQUIRE( *x2 == 10l );
+  REQUIRE( *x3 == 10l );
+
+}
+
+
+TEST_CASE( "Symbol garnishing", "[garnish]" ) {
+
+  Symbolic sym = Symbols::get()["foo"];
+
+  ObjectPtr obj = garnishObject(globalVM->reader, sym);
+  Symbolic* sym1 = boost::get<Symbolic>(&obj->prim());
+
+  REQUIRE( sym1 != nullptr );
+  REQUIRE( *sym1 == sym );
+
+}
+
+TEST_CASE( "Singleton garnishing (Booleans and nil)", "[garnish]" ) {
+
+  ObjectPtr t = garnishObject(globalVM->reader, true);
+  ObjectPtr f = garnishObject(globalVM->reader, false);
+  ObjectPtr n = garnishObject(globalVM->reader, boost::blank());
+
+  REQUIRE( t == globalVM->reader.lit[Lit::TRUE] );
+  REQUIRE( f == globalVM->reader.lit[Lit::FALSE] );
+  REQUIRE( n == globalVM->reader.lit[Lit::NIL] );
+
+}
