1aeb2ec2df588dc5df10f4a83376f4b9bd574696
Silvio Mayolo <sdmayolo42@students.tntech.edu>
2018-07-28 22:46:15 -0500

    Allocator and GC tests

---
+++ test/test_Allocator.cpp
@@ -0,0 +1,44 @@
+//// Copyright (c) 2018 Silvio Mayolo
+//// See LICENSE.txt for licensing details
+
+#include "catch2/catch.hpp"
+#include "test.hpp"
+#include "Allocator.hpp"
+#include "GC.hpp"
+
+TEST_CASE( "Allocation and deallocation work as expected", "" ) {
+
+  // Note that we're testing the allocator here, but allocation needs
+  // to go through the garbage collector for bookkeeping purposes.
+  // Messy? Yes.
+
+  ObjectPtr newObject = GC::get().allocate();
+
+  REQUIRE( newObject != nullptr );
+
+  // Deallocates by ref count
+
+}
+
+TEST_CASE( "Deallocation by ref count actually happens", "" ) {
+
+  ObjectPtr newObject0 = GC::get().allocate();
+
+  REQUIRE( newObject0 != nullptr );
+
+  Object* raw = newObject0.get();
+  REQUIRE( reinterpret_cast<ObjectEntry*>(raw)->in_use == true );
+
+  {
+
+    // We're going to force the variable to go out of scope inside
+    // this block.
+
+    ObjectPtr stable = newObject0; // Ref count = 2
+    newObject0 = nullptr;          // Ref count = 1
+  }                                // Ref count = 0
+
+  REQUIRE( reinterpret_cast<ObjectEntry*>(raw)->in_use == false );
+
+}
+
---
+++ test/test_GC.cpp
@@ -0,0 +1,34 @@
+//// Copyright (c) 2018 Silvio Mayolo
+//// See LICENSE.txt for licensing details
+
+#include "catch2/catch.hpp"
+#include "test.hpp"
+#include "Allocator.hpp"
+#include "GC.hpp"
+
+TEST_CASE( "The garbage collector can be run by force", "" ) {
+
+  long count = GC::get().garbageCollect(*globalVM);
+  REQUIRE( count >= 0 );
+
+}
+
+TEST_CASE( "The garbage collector cleans up cyclic references", "" ) {
+
+  ObjectPtr obj = clone(globalVM->reader.lit[Lit::OBJECT]);
+  Object* raw = obj.get();
+  ObjectEntry* data = reinterpret_cast<ObjectEntry*>(raw);
+
+  obj->put(Symbols::get()["cyclicReference"], obj);
+  obj = nullptr;
+
+  // The object still retains a cyclic reference to itself, so it
+  // can't be cleaned up by reference counting alone.
+  REQUIRE( data->in_use );
+
+  // However, it can be cleaned up by the GC
+  long count = GC::get().garbageCollect(*globalVM);
+  REQUIRE( count >= 1 );
+  REQUIRE( !data->in_use );
+
+}
